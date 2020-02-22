372d98a9f42b6a240547f469a3ed16a33f237756
hairarrow <emmanuelherrero@gmail.com>
2020-02-21 17:36:56 -0800

    Conditionally display pagination buttons

--- src/App.js
+++ src/App.js
@@ -60,8 +60,22 @@ const pages_data = [
         rowId: "1-0",
         panels: [
           {
-            panelId: "1-0-0k",
+            panelId: "1-0-0",
             description: "A dark, lonely, void"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    pageId: 2,
+    rows: [
+      {
+        rowId: "2-0",
+        panels: [
+          {
+            panelId: "2-0-0",
+            description: "A slightly different, dark, lonely void"
           }
         ]
       }
@@ -93,8 +107,11 @@ function ComicBook() {
   const [currentPageId, setCurrentPageId] = useState(0);
   const pageData = getPageFromId(currentPageId);
 
+  const isFirstPage = () => currentPageId === 0;
+  const isLastPage = () => currentPageId === pages_data.length - 1;
+
   function prevPage() {
-    if (currentPageId === 0) {
+    if (isFirstPage()) {
       return;
     }
 
@@ -102,7 +119,7 @@ function ComicBook() {
   }
 
   function nextPage() {
-    if (currentPageId === pages_data.length - 1) {
+    if (isLastPage()) {
       return;
     }
 
@@ -119,76 +136,93 @@ function ComicBook() {
   };
 
   return (
-    <article
+    <main
       style={{
-        background: "#000",
         minWidth: "100vw",
         minHeight: "100vh",
-        display: "grid",
-        gridTemplateRows: "1fr auto"
+        display: "flex",
+        justifyContent: "center",
+        background: "#323334"
       }}
     >
-      <section
+      <article
         style={{
+          maxWidth: 1200,
+          width: "100%",
+          minHeight: "100%",
           display: "grid",
-          padding: gridGap,
-          gridTemplateRows: `${frPerArrayItem(pageData.rows)}`,
-          gridGap
+          gridTemplateRows: "1fr auto"
         }}
       >
-        {pageData.rows.map((row) => (
-          <div
-            key={row.Id}
-            style={{
-              display: "grid",
-              gridTemplateColumns: `${frPerArrayItem(row.panels)}`,
-              gridGap
-            }}
-          >
-            {row.panels.map((panel) => (
-              <div
-                key={panel.id}
-                style={{
-                  background: "#fff",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  flexDirection: "column",
-                  flex: 1,
-                  padding: gridGap,
-                  lineHeight: 1.6
-                }}
-              >
-                <p style={{ color: "rgba(0, 0, 0, .5)" }}>
-                  {panel.description}
-                </p>
-                {panel.messages &&
-                  panel.messages.map((message, messageId) => (
-                    <div key={{ messageId }} style={{ fontSize: 24 }}>
-                      {getCharacterEmoji(message.character)}: {message.text}
-                    </div>
-                  ))}
-              </div>
-            ))}
-          </div>
-        ))}
-      </section>
-      <footer
-        style={{
-          paddingBottom: gridGap,
-          paddingRight: gridGap,
-          display: "flex",
-          justifyContent: "flex-end"
-        }}
-      >
-        <button style={{ marginRight: 24, ...buttonStyles }} onClick={prevPage}>
-          Previous Page
-        </button>
-        <button style={buttonStyles} onClick={nextPage}>
-          Next Page
-        </button>
-      </footer>
-    </article>
+        <section
+          style={{
+            display: "grid",
+            padding: gridGap,
+            gridTemplateRows: `${frPerArrayItem(pageData.rows)}`,
+            gridGap
+          }}
+        >
+          {pageData.rows.map((row) => (
+            <div
+              key={row.Id}
+              style={{
+                display: "grid",
+                gridTemplateColumns: `${frPerArrayItem(row.panels)}`,
+                gridGap
+              }}
+            >
+              {row.panels.map((panel) => (
+                <div
+                  key={panel.id}
+                  style={{
+                    background: "#fff",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    flexDirection: "column",
+                    flex: 1,
+                    padding: gridGap,
+                    lineHeight: 1.6,
+                    borderRadius: 8
+                  }}
+                >
+                  <p style={{ color: "rgba(0, 0, 0, .5)" }}>
+                    {panel.description}
+                  </p>
+                  {panel.messages &&
+                    panel.messages.map((message, messageId) => (
+                      <div key={{ messageId }} style={{ fontSize: 24 }}>
+                        {getCharacterEmoji(message.character)}: {message.text}
+                      </div>
+                    ))}
+                </div>
+              ))}
+            </div>
+          ))}
+        </section>
+        <footer
+          style={{
+            paddingBottom: gridGap,
+            paddingRight: gridGap,
+            display: "grid",
+            justifyContent: "flex-end",
+            gridTemplateColumns: "auto auto",
+            ...(!isFirstPage() && !isLastPage() && { gridGap })
+          }}
+        >
+          {!isFirstPage() && (
+            <button style={buttonStyles} onClick={prevPage}>
+              Previous Page
+            </button>
+          )}
+          {!isLastPage() && (
+            <button style={buttonStyles} onClick={nextPage}>
+              Next Page
+            </button>
+          )}
+        </footer>
+      </article>
+    </main>
   );
 }
 
